86c9b06f434afcaff110fb2ff9eb02e47823b1cf
Jirayu Limjinda <jir4yu.l@gmail.com>
2018-03-02 15:22:47 +0700

    fixed company name in footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 						<div class="col-sm-6">
 							<img src="<?php echo get_template_directory_uri() ?>/img/jindatheme-logo-black.png" class="footer-logo" alt="<?php bloginfo('name') ?>" />
 							<div class="footer-company">
-								<p><?php _e('JindaTheme', 'jindatheme') ?></p>
+								<p><?php _e('JindaTheme Company Limited', 'jindatheme') ?></p>
 								<p><small><?php _e('email', 'jindatheme') ?>: <?php the_field('email', 'option') ?></small></p>
 							</div>
 						</div>
